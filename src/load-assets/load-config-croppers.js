a7b591b4e9f53f3c3d866be7e3b0d990137ed4c8
tommyZZM <452490446@qq.com>
2020-03-04 23:06:52 +0800

    src/load-assets: increase load speed

--- src/load-assets/load-config-croppers.js
+++ src/load-assets/load-config-croppers.js
@@ -1,8 +1,10 @@
 import axios from "axios"
+import json from "../../config_source_to_fragments_famale_croppers.json"
 
 const URL_TO_CONFIG_CROPPERS = window.location.origin + window.location.pathname + '/config_source_to_fragments_famale_croppers.json';
 
 export default async function() {
-  const response = await axios.get(URL_TO_CONFIG_CROPPERS);
-  return response.data;
+  return json;
+  // const response = await axios.get(URL_TO_CONFIG_CROPPERS);
+  // return response.data;
 }
--- src/load-assets/load-config-output.js
+++ src/load-assets/load-config-output.js
@@ -1,8 +1,10 @@
 import axios from "axios"
+import json from "../../config_output_paths.json"
 
 const URL_TO_CONFIG_OUTPUTS = window.location.origin + window.location.pathname + '/config_output_paths.json';
 
 export default async function() {
-  const response = await axios.get(URL_TO_CONFIG_OUTPUTS);
-  return response.data;
+  return json;
+  // const response = await axios.get(URL_TO_CONFIG_OUTPUTS);
+  // return response.data;
 }
